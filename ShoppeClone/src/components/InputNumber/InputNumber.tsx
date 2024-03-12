beb936a5e22b620de6bfb078c0dec5ffb586acca
Coder6pack <105229432+Coder6pack@users.noreply.github.com>
2024-03-12 11:11:20 +0700

    Update InputNumber

--- ShoppeClone/src/components/InputNumber/InputNumber.tsx
+++ ShoppeClone/src/components/InputNumber/InputNumber.tsx
@@ -1,4 +1,4 @@
-import { InputHTMLAttributes, forwardRef } from 'react'
+import { InputHTMLAttributes, forwardRef, useState } from 'react'
 export interface InputNumberProps extends InputHTMLAttributes<HTMLInputElement> {
   errorMessage?: string
   autoComplete?: string
@@ -12,19 +12,25 @@ const InputNumber = forwardRef<HTMLInputElement, InputNumberProps>(function Inpu
     classNameInput = 'p-3 rounded-sm border border-gray-300 focus:border-gray-500 focus:shadow-sm w-full outline-none',
     classNameError = 'mt-1 min-h-[1rem] text-sm text-red-600',
     onChange,
+    value = '',
     ...rest
   },
   ref
 ) {
+  const [localValue, setLocalValue] = useState<string>(value as string)
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target
-    if ((/^\d+$/.test(value) || value === '') && onChange) {
-      onChange(event)
+    if (/^\d+$/.test(value) || value === '') {
+      // Thuc thi call back onChange
+      onChange && onChange(event)
+
+      // set localValue
+      setLocalValue(value)
     }
   }
   return (
     <div className={className}>
-      <input className={classNameInput} onChange={handleChange} {...rest} ref={ref} />
+      <input className={classNameInput} onChange={handleChange} value={value || localValue} {...rest} ref={ref} />
       <div className={classNameError}>{errorMessage}</div>
     </div>
   )
--- ShoppeClone/src/components/QuantityController/QuantityController.tsx
+++ ShoppeClone/src/components/QuantityController/QuantityController.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import InputNumber, { InputNumberProps } from '../InputNumber'
 
 interface Props extends InputNumberProps {
@@ -10,13 +10,14 @@ interface Props extends InputNumberProps {
 }
 export default function QuantityController({
   max,
-  value,
+  value = '',
   classNameWrapper = 'ml-10',
   onIncrease,
   onDecrease,
   onType,
   ...rest
 }: Props) {
+  const [localValue, setLocalValue] = useState<number>(Number(value || 1))
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     let _value = Number(event.target.value)
     if (max !== undefined && _value > max) {
@@ -26,23 +27,27 @@ export default function QuantityController({
     }
 
     onType && onType(_value)
+
+    setLocalValue(_value)
   }
 
   const inCrease = () => {
-    let _value = Number(value) + 1
+    let _value = Number(value || localValue) + 1
     if (max !== undefined && _value > max) {
       _value = max
     }
     onIncrease && onIncrease(_value)
+    setLocalValue(_value)
   }
 
   const decrease = () => {
-    let _value = Number(value) - 1
+    let _value = Number(value || localValue) - 1
     if (_value < 1) {
       _value = 1
     }
 
     onDecrease && onDecrease(_value)
+    setLocalValue(_value)
   }
   return (
     <div className={'flex items-center ' + classNameWrapper}>
@@ -65,7 +70,7 @@ export default function QuantityController({
         classNameInput='h-8 w-14 border-t border-b border-gray-300 p-1 text-center outline-none'
         classNameError='hidden'
         onChange={handleChange}
-        value={value}
+        value={value || localValue}
         {...rest}
       />
       <button
